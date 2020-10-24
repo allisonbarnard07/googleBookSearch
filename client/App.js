1ef5ab319d94b44e4f3992bb770deb1f39344e4d
Allison Barnard <allisonbarnard07@gmail.com>
2020-10-23 20:59:07 -0400

    updated need to fix bug of index.html not found

--- client/App.js
+++ client/App.js
@@ -2,10 +2,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Nav from "./components/Nav"
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
-import Jumbotron from "./components/Jumbotron";
+import Nav from "./src/components/Nav"
+import Search from "./pages/SearchBooks";
+import Saved from "./pages/SavedBooks";
+import Jumbotron from "./src/components/Jumbotron";
 
 class App extends Component {
   render() {
@@ -15,11 +15,11 @@ class App extends Component {
           <Nav />
           <Jumbotron />
           <Switch>
-            {/* renders the Search page when "/" route is hit */}
+            {}
             <Route exact path="/" component={Search} />
-            {/* renders the Search page when "/search" route is hit */}
+            {}
             <Route exact path="/search" component={Search} />
-            {/* renders the Saved page when "/saved" route is hit */}
+            {}
             <Route exact path="/saved" component={Saved} />
           </Switch>
         </div>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// endpoint from googlebooks api
+
 const BaseUrl = "https://www.googleapis.com/books/v1/volumes?q="
 
 export default {
